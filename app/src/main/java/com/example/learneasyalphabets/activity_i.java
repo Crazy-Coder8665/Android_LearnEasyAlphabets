51158e3284cbd1c2c50cd712724f4f6677345942
Talha Masood <talha8665i@gmail.com>
2022-05-22 18:56:28 +0500

    Activity i to l created

---
+++ app/src/main/java/com/example/learneasyalphabets/activity_i.java
@@ -0,0 +1,24 @@
+package com.example.learneasyalphabets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_i extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i);
+    }
+    public void next(View view) {
+        Intent intent = new Intent(this,activity_j.class);
+        startActivity(intent);
+    }
+    public void prev(View view) {
+        Intent intent = new Intent(this,activity_h.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/learneasyalphabets/activity_l.java
@@ -0,0 +1,24 @@
+package com.example.learneasyalphabets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_l extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l);
+    }
+    public void next(View view) {
+        Intent intent = new Intent(this,activity_m.class);
+        startActivity(intent);
+    }
+    public void prev(View view) {
+        Intent intent = new Intent(this,activity_k.class);
+        startActivity(intent);
+    }
+}
