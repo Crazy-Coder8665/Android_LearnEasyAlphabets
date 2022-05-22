688948a108216f3a95b1278297a07efcbb34dfcf
Talha Masood <talha8665i@gmail.com>
2022-05-22 17:11:34 +0500

    Layout Created

--- app/src/main/java/com/example/learneasyalphabets/activity_a.java
+++ app/src/main/java/com/example/learneasyalphabets/activity_a.java
@@ -3,17 +3,12 @@ package com.example.learneasyalphabets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_a extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void intent_a(View view) {
-
+        setContentView(R.layout.activity_a);
     }
 }
