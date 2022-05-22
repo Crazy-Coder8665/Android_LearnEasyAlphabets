25598e470b61aef6a14972e36f0f0abd75dcaf9b
Talha Masood <talha8665i@gmail.com>
2022-05-22 17:26:21 +0500

    Events Created

--- app/src/main/java/com/example/learneasyalphabets/MainActivity.java
+++ app/src/main/java/com/example/learneasyalphabets/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.learneasyalphabets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,111 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void intent_a(View view) {
-
+        Intent intent = new Intent(this,activity_a.class);
+        startActivity(intent);
+    }
+    public void intent_b(View view) {
+        Intent intent = new Intent(this,activity_b.class);
+        startActivity(intent);
+    }
+    public void intent_c(View view) {
+        Intent intent = new Intent(this,activity_c.class);
+        startActivity(intent);
+    }
+    public void intent_d(View view) {
+        Intent intent = new Intent(this,activity_d.class);
+        startActivity(intent);
+    }
+    public void intent_e(View view) {
+        Intent intent = new Intent(this,activity_e.class);
+        startActivity(intent);
+    }
+    public void intent_f(View view) {
+        Intent intent = new Intent(this,activity_f.class);
+        startActivity(intent);
+    }
+    public void intent_g(View view) {
+        Intent intent = new Intent(this,activity_g.class);
+        startActivity(intent);
+    }
+    public void intent_h(View view) {
+        Intent intent = new Intent(this,activity_h.class);
+        startActivity(intent);
+    }
+    public void intent_i(View view) {
+        Intent intent = new Intent(this,activity_i.class);
+        startActivity(intent);
+    }
+    public void intent_j(View view) {
+        Intent intent = new Intent(this,activity_j.class);
+        startActivity(intent);
+    }
+    public void intent_k(View view) {
+        Intent intent = new Intent(this,activity_k.class);
+        startActivity(intent);
+    }
+    public void intent_l(View view) {
+        Intent intent = new Intent(this,activity_l.class);
+        startActivity(intent);
+    }
+    public void intent_m(View view) {
+        Intent intent = new Intent(this,activity_m.class);
+        startActivity(intent);
+    }
+    public void intent_n(View view) {
+        Intent intent = new Intent(this,activity_n.class);
+        startActivity(intent);
+    }
+    public void intent_o(View view) {
+        Intent intent = new Intent(this,activity_o.class);
+        startActivity(intent);
+    }
+    public void intent_p(View view) {
+        Intent intent = new Intent(this,activity_p.class);
+        startActivity(intent);
+    }
+    public void intent_q(View view) {
+        Intent intent = new Intent(this,activity_q.class);
+        startActivity(intent);
+    }
+    public void intent_r(View view) {
+        Intent intent = new Intent(this,activity_r.class);
+        startActivity(intent);
+    }
+    public void intent_s(View view) {
+        Intent intent = new Intent(this,activity_s.class);
+        startActivity(intent);
+    }
+    public void intent_t(View view) {
+        Intent intent = new Intent(this,activity_t.class);
+        startActivity(intent);
+    }
+    public void intent_u(View view) {
+        Intent intent = new Intent(this,activity_u.class);
+        startActivity(intent);
+    }
+    public void intent_v(View view) {
+        Intent intent = new Intent(this,activity_v.class);
+        startActivity(intent);
+    }
+    public void intent_w(View view) {
+        Intent intent = new Intent(this,activity_w.class);
+        startActivity(intent);
+    }
+    public void intent_x(View view) {
+        Intent intent = new Intent(this,activity_x.class);
+        startActivity(intent);
+    }
+    public void intent_y(View view) {
+        Intent intent = new Intent(this,activity_y.class);
+        startActivity(intent);
+    }
+    public void intent_z(View view) {
+        Intent intent = new Intent(this,activity_z.class);
+        startActivity(intent);
+    }
+    public void intent_startQuiz(View view) {
+        Intent intent = new Intent(this,activity_startQuiz.class);
+        startActivity(intent);
     }
 }
